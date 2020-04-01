f86f79cfae5a53cfd7fd907c930aedb1b6a9526c
znerol <lo+github@znerol.ch>
2020-04-01 15:21:45 +0200

    chore(ci): Use Xcode 9.2 / macOS Sierra as build system

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/znerol/socketwebify", from: "1.0.0"),
+        .package(url: "https://github.com/znerol/socketwebify", from: "1.0.1"),
     ],
     targets: [
         .target(name: "novnc-connect", dependencies: [
